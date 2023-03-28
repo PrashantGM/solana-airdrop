57b12437a6dbedb042322d87dbdb9e489d197ae0
Prashant Gharti Magar <prashant.ghartimagar@tai.com.np>
2023-03-28 23:04:21 +0545

    feat: remove testnet option

--- app.ts
+++ app.ts
@@ -11,7 +11,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', router);
 
